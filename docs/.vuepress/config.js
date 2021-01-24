03a9e8f2c3e4a0ef630d78228b5c7feef9b0477d
lotosv2010 <lotosv2010@163.com>
2021-01-24 18:16:30 +0800

    docs(vuepress): add vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'vue-project-template',
+  description: 'vue 项目模板',
+  themeConfig: {
+    theme: 'reco',
+    type: 'blog',
+    logo: '/img/logo.png',
+    sidebar: 'auto',
+    displayAllHeaders: true,
+    lastUpdated: 'Last Updated',
+    repo: 'vuejs/vuepress',
+    repoLabel: 'vuepress',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+    smoothScroll: true,
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: 'cli', link: '/cli' }
+    ]
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    '@vuepress/back-to-top',
+    '@vuepress/nprogress'
+  ]
+};
